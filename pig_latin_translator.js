06c9e4574059b637f1874d600a9f4eb8cc390221
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-19 05:17:58 -0400

    improve the Pig Latin Translator, ensuring the translation based on the three rules outlined by Purdue University is correctly working.

--- pig_latin_translator.js
+++ pig_latin_translator.js
@@ -1,13 +1,13 @@
-let singleWordInput = 'Field'.toUpperCase();
+let singleWordInput = 'Flanders'.toUpperCase();
 let pigLatinConversion = [...singleWordInput];
 let vowels = ['A', 'E', 'I', 'O', 'U'];
 
 for (let i = 0; i < vowels.length; i++) {
-    if (vowels[i] === pigLatinConversion[0]) {
+    if (pigLatinConversion[0] === 'A' || pigLatinConversion[0] === 'E' || pigLatinConversion[0] === 'I' || pigLatinConversion[0] === 'O' || pigLatinConversion[0] === 'U') {
         pigLatinConversion.push('W','A','Y');
         break;
     };
-    if (pigLatinConversion[0] !== vowels[i] && pigLatinConversion[1] === vowels[i]) {
+    if (pigLatinConversion[0] !== vowels[i] && pigLatinConversion[1] === 'A' || pigLatinConversion[1] === 'E' || pigLatinConversion[1] === 'I' || pigLatinConversion[1] === 'O' || pigLatinConversion[1] === 'U') {
         let convertThree = pigLatinConversion.shift();
         console.log(convertThree)
         pigLatinConversion.push(convertThree);
@@ -24,4 +24,4 @@ for (let i = 0; i < vowels.length; i++) {
     };
 }
 
-console.log(pigLatinConversion);
+console.log(pigLatinConversion.join(''));
