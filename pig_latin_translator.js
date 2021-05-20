2f6e38f792775df132c5778d8dc01050c927b6f7
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-20 15:31:07 -0400

    add functionality to translate basic sentences to Pig Latin.

--- pig_latin_translator.js
+++ pig_latin_translator.js
@@ -1,6 +1,45 @@
-let singleWordInput = 'Flanders'.toUpperCase();
-let pigLatinConversion = [...singleWordInput];
 let vowels = ['A', 'E', 'I', 'O', 'U'];
+let regex = /^[a-z]/;
+let singleWordInput = 'Flanders'.toUpperCase();
+let sentenceInput = 'Pig Latin is hard to speak'.toUpperCase();
+let pigLatinSentence = sentenceInput.split(' ');
+let pigLatinSentenceBroken = pigLatinSentence.map(word => {
+    let x = [...word];
+    for (let i = 0; i < vowels.length; i++) {
+        if (x[0] === 'A' || x[0] === 'E' || x[0] === 'I' || x[0] === 'O' || x[0] === 'U') {
+            x.push('W','A','Y');
+            break;
+        };
+        if (x[0] !== vowels[i] && x[1] === 'A' || x[1] === 'E' || x[1] === 'I' || x[1] === 'O' || x[1] === 'U') {
+            let convertThree = x.shift();
+            x.push(convertThree);
+            x.push('A', 'Y');
+            break;
+        };
+        if (x[0] !== vowels[i] && x[1] !== vowels[i]) {
+            let convert = x.shift();
+            let convertTwo = x.shift();
+            x.push(convert);
+            x.push(convertTwo);
+            x.push('A', 'Y');
+            break;
+        };
+    }
+    let y = x.join('').toLowerCase();
+    return y;
+});
+let boundPigLatinSentence = pigLatinSentenceBroken.join(' ');
+let capitalizedSentence = boundPigLatinSentence[0].toUpperCase();
+let renderedPigLatinSentence = boundPigLatinSentence.replace(regex, capitalizedSentence);
+
+
+console.log(pigLatinSentenceBroken);
+console.log(boundPigLatinSentence);
+console.log(renderedPigLatinSentence);
+
+
+
+let pigLatinConversion = [...singleWordInput];
 
 for (let i = 0; i < vowels.length; i++) {
     if (pigLatinConversion[0] === 'A' || pigLatinConversion[0] === 'E' || pigLatinConversion[0] === 'I' || pigLatinConversion[0] === 'O' || pigLatinConversion[0] === 'U') {
@@ -26,7 +65,6 @@ for (let i = 0; i < vowels.length; i++) {
 
 let pigLatin = pigLatinConversion.join('').toLowerCase();
 let capitalPigLatin = pigLatin[0].toUpperCase();
-let regex = /^[a-z]/;
 let finalPigLatin = pigLatin.replace(regex, capitalPigLatin);
 
 console.log(finalPigLatin);
