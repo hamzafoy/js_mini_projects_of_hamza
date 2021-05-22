aeb9f0bb4e89ce3ec1289f9977c8eb17b48ac4ad
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-22 05:20:44 -0400

    improve the sentence translation by combing through and removing common punctuation marks that would otherwise affect the translation quality.

--- pig_latin_translator.js
+++ pig_latin_translator.js
@@ -4,7 +4,7 @@ let vowels = ['A', 'E', 'I', 'O', 'U'];
 let regex = /^[a-z]/;
 // The single word input that will be translated & the basic sentence that will also be translated.
 let singleWordInput = 'Flanders'.toUpperCase();
-let sentenceInput = 'My name is Hamza'.toUpperCase();
+let sentenceInput = 'Hello, My name is Hamza'.toUpperCase();
 
 
 
@@ -21,31 +21,40 @@ let pigLatinSentence = sentenceInput.split(' ');
 //Rule #3: If the word has a consonant for its first letter & a vowel for its second letter, then remove the first letter only & place it at the end of the word, then add 'ay'.
 let pigLatinSentenceBroken = pigLatinSentence.map(word => {
     let x = [...word];
+    //The `w` variable can be deleted, replace `w` in the following FOR LOOP with `x`. The `w` variable is the filter removing punctuation to ensure proper translation.
+    let w = x.filter(letter => {
+        if (letter === ',' || letter === '.' || letter === "'") {
+            
+        } else {
+            return letter;
+        }
+    });
+    console.log(w);
     for (let i = 0; i < vowels.length; i++) {
         //Rule #1 & corresponding changes.
-        if (x[0] === 'A' || x[0] === 'E' || x[0] === 'I' || x[0] === 'O' || x[0] === 'U') {
-            x.push('W','A','Y');
+        if (w[0] === 'A' || w[0] === 'E' || w[0] === 'I' || w[0] === 'O' || w[0] === 'U') {
+            w.push('W','A','Y');
             break;
         };
         //Rule #3 & corresponding changes.
-        if (x[0] !== vowels[i] && x[1] === 'A' || x[1] === 'E' || x[1] === 'I' || x[1] === 'O' || x[1] === 'U') {
-            let convertThree = x.shift();
-            x.push(convertThree);
-            x.push('A', 'Y');
+        if (w[0] !== vowels[i] && w[1] === 'A' || w[1] === 'E' || w[1] === 'I' || w[1] === 'O' || w[1] === 'U') {
+            let convertThree = w.shift();
+            w.push(convertThree);
+            w.push('A', 'Y');
             break;
         };
         //Rule #2 & corresponding changes.
-        if (x[0] !== vowels[i] && x[1] !== vowels[i]) {
-            let convert = x.shift();
-            let convertTwo = x.shift();
-            x.push(convert);
-            x.push(convertTwo);
-            x.push('A', 'Y');
+        if (w[0] !== vowels[i] && w[1] !== vowels[i]) {
+            let convert = w.shift();
+            let convertTwo = w.shift();
+            w.push(convert);
+            w.push(convertTwo);
+            w.push('A', 'Y');
             break;
         };
     }
     //Placing each word, its letters joined back together again, in an array.
-    let y = x.join('').toLowerCase();
+    let y = w.join('').toLowerCase();
     return y;
 });
 //Joining the words back together in a full sentence.
