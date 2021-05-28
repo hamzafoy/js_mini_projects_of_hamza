5c6235f8aec33347ccea89205672eb61cc1c4773
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-28 04:08:52 -0400

    add another project - a library mgr exploring classes and extending classes.

---
+++ library_organizer.js
@@ -0,0 +1,52 @@
+class Media {
+    constructor(title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
+    }
+    get title() {
+        return this._title;
+    }
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+    get ratings() {
+        return this._ratings;
+    }
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+    getAverageRating() {
+        const reduce = (acc, cur) => acc + cur;
+        let reduction = this._ratings.reduce(reduce);
+        return reduction / this._ratings.length;
+    }
+    addRating(score) {
+        this._ratings.push(score);
+    }
+};
+
+
+
+class Book extends Media {
+    constructor(title, author, pages, isCheckedOut, ratings) {
+        super(title, isCheckedOut, ratings);
+        this._author = author;
+        this._pages = pages;
+    }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
+    }
+}
+
+
+
+const NineteenEightyFour = new Book('1984', 'George Orwell', 328);
+NineteenEightyFour.addRating(8.5);
+NineteenEightyFour.addRating(9);
+NineteenEightyFour.addRating(8.5);
+
+console.log(NineteenEightyFour); // EXPECTED OUTPUT: Book { _title: '1984', _isCheckedOut: false, _ratings[ 8.5, 9, 8.5 ], _author: 'George Orwell', _pages: 328 }
