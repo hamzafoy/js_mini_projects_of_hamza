9f264aa80a17f3a3ccbc8e67d39e7f9f1cd230a8
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-04 21:45:00 -0500

    added a function that took unk # of params with wind speeds and averaged them out.

---
+++ wind_speed_calc.js
@@ -0,0 +1,28 @@
+const averageWindSpeed = (weatherEntries) => {
+    let averageWinds = 0;
+    let numOfEntries = 0;
+        for(let i in weatherEntries) {
+            averageWinds += weatherEntries[i].windSpeed;
+            numOfEntries++;
+        }
+    averageWinds = Math.round(averageWinds / numOfEntries);
+    return averageWinds;
+}
+
+const testWeatherEntries = [
+    {
+        windSpeed: 25,
+        condition: "rainy"
+    },
+    {
+        windSpeed: 12,
+        condition: "cloudy"
+    },
+    {
+        windSpeed: 5.5,
+        condition: "sunny"
+    }
+]
+
+
+console.log(averageWindSpeed(testWeatherEntries));
