bb29148542c1e1292c092bb72038419ccf53dfb6
Ross Foy <hamzaoflouisville@gmail.com>
2022-01-01 04:58:31 -0500

    building out an interactive terminal program to review a website hosting plan.

--- comparingHostingPlans.js
+++ comparingHostingPlans.js
@@ -13,6 +13,10 @@ class hostingPlan {
         this.storageSpace = storageSpace;
         this.numOfWebPages = numOfWebPages;
     }
+
+    calculateHourlyRate(numOfHours) {
+        return 
+    }
 }
 
 let basicHosting = new hostingPlan('Basic Hosting', 3.99, 100, 5);
@@ -24,8 +28,20 @@ const rl = readline.createInterface({
 });
 
 rl.question(`What is your name? `, function(name) {
-    console.log(`Hello ${name}, please view our basic hosting plan: ${basicHosting.name} with a low price of ${basicHosting.ratePerHour}!`);
-    rl.close();
+    rl.question(`Type 'A' to review plan's storage space, type 'B' to calculate estimated costs for plan`, function(option) {
+        switch (option == 'A') {
+            case true:
+            console.log(`You can expect to have ${basicHosting.storageSpace}MB of space with this plan!`);
+            rl.close();
+            case false:
+                rl.question(`How many hours are you estimating your website would cost to develop? `, function(answer) {
+                    let calculations = (3.99 * answer);
+                    console.log(`Expect to pay upwards of ${calculations} dollars.`);
+                    rl.close();
+                })
+        }
+    })
+    //console.log(`Hello ${name}, please view our basic hosting plan: ${basicHosting.name} with a low price of ${basicHosting.ratePerHour}!`);
 });
 
 rl.on('close', function() {
