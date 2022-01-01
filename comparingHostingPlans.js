fa4ea5a366f8d4e8a279f9d51f13ac69d74ba12c
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-31 23:35:19 -0500

    add a new mini project related to objects.

---
+++ comparingHostingPlans.js
@@ -0,0 +1,7 @@
+/**
+ * The following objects will represent fictional
+ * website hosting plans and contain properties and
+ * methods that contain information and handle calculations
+ * so someone can compare plans.
+ *
+ */
