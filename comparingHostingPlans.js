50665b8f81b58cf723dd5b84d61ccc1303fc7de8
Ross Foy <hamzaoflouisville@gmail.com>
2022-01-01 02:08:23 -0500

    Testing out readline built-on module to get input from terminal.

--- comparingHostingPlans.js
+++ comparingHostingPlans.js
@@ -5,3 +5,19 @@
  * so someone can compare plans.
  *
  */
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(`What is your name?`, function(name) {
+    console.log(`Hello ${name}, welcome to the Node plan processor!`);
+    rl.close();
+});
+
+rl.on('close', function() {
+    console.log(`This is Node planning processor closing, Goodbye!`);
+    process.exit(0);
+})
